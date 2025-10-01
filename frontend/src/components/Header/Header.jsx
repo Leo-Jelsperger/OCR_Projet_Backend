7c7a07633483fce5c282b1dedfe87bbee68162a8
Leo-Jelsperger <158077569+Leo-Jelsperger@users.noreply.github.com>
2025-10-01 17:13:52 +0200

    end of project

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
-import { NavLink, useNavigate } from 'react-router-dom';
+import {NavLink, useNavigate} from 'react-router-dom';
 
 import * as PropTypes from 'prop-types';
 import styles from './Header.module.css';
 import Logo from '../../images/Logo.png';
 
-function Header({ user, setUser }) {
+function Header({user, setUser}) {
   const navigate = useNavigate();
   const disconnect = () => {
     localStorage.clear();
@@ -16,11 +16,49 @@ function Header({ user, setUser }) {
   return (
     <header className={styles.Header}>
       <div className="container">
-        <img src={Logo} alt="logo mpm vieu grimoire" />
+        <img
+          src={Logo}
+          alt="logo mpm vieu grimoire"
+        />
         <ul>
-          <li><NavLink to="/" end className={({ isActive }) => (isActive ? styles.activeLink : undefined)}>Accueil</NavLink></li>
-          <li><NavLink to="/Ajouter" className={({ isActive }) => (isActive ? styles.activeLink : undefined)}>Ajouter un livre</NavLink></li>
-          <li>{!user ? <NavLink to="/Connexion" className={({ isActive }) => (isActive ? styles.activeLink : undefined)}>Se connecter</NavLink> : <span tabIndex={0} role="button" onKeyUp={disconnect} onClick={disconnect}>Se déconnecter</span> }</li>
+          <li>
+            <NavLink
+              to="/"
+              end
+              className={({isActive}) =>
+                isActive ? styles.activeLink : undefined
+              }>
+              Accueil
+            </NavLink>
+          </li>
+          <li>
+            <NavLink
+              to="/Ajouter"
+              className={({isActive}) =>
+                isActive ? styles.activeLink : undefined
+              }>
+              Ajouter un livre
+            </NavLink>
+          </li>
+          <li>
+            {!user ? (
+              <NavLink
+                to="/Connexion"
+                className={({isActive}) =>
+                  isActive ? styles.activeLink : undefined
+                }>
+                Se connecter
+              </NavLink>
+            ) : (
+              <span
+                tabIndex={0}
+                role="button"
+                onKeyUp={disconnect}
+                onClick={disconnect}>
+                Se déconnecter
+              </span>
+            )}
+          </li>
         </ul>
       </div>
     </header>
